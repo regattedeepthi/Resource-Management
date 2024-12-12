82d9d54dccd9238118f57401e15c9ae34b0474ef
regattedeepthi <regattedeepthi@gmail.com>
2024-12-12 15:56:16 +0530

    Resource management

---
+++ construction/construction_app/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import *
+ 
+class ProjectSerializer(serializers.ModelSerializer):
+    labour_ids = serializers.ListField(child=serializers.IntegerField())
+    material_id = serializers.ListField(child=serializers.IntegerField())
+    material_quantity = serializers.ListField(child=serializers.IntegerField())
+    equipment_id = serializers.ListField(child=serializers.IntegerField())
+    equipment_quantity = serializers.ListField(child=serializers.IntegerField())
+ 
+    class Meta:
+        model = Project
+        fields = ['id', 'project_name', 'labour_ids', 'material_id', 'material_quantity', 'equipment_id', 'equipment_quantity']
+ 
+class LabourSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Labour
+        fields = ['id', 'labour_name','profession','profession_name','allocated']
+ 
+class EquipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Equipment
+        fields = ['id', 'equipment_name', 'quantity']
+ 
+class MaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Material
+        fields = ['id', 'material_name', 'quantity','units']
+       
+class ProfessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profession
+        fields = ['id', 'name']
