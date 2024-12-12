45cc090bbe30bdd00d2980910f447845f13ba25a
regattedeepthi <regattedeepthi@gmail.com>
2024-12-12 17:01:34 +0530

    Updated urls.py

--- construction/construction_app/urls.py
+++ construction/construction_app/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('projects/', views.ProjectView.as_view()),
+    path('project/', views.ProjectView.as_view()),
     path('labours/', views.LabourView.as_view()),
     path('labours/<int:pk>/', views.LabourView.as_view()),
     path('equipments/', views.EquipmentView.as_view()),
     path('equipments/<int:pk>/', views.EquipmentView.as_view()),
     path('materials/<int:pk>/', views.MaterialView.as_view()),
     path('materials/', views.MaterialView.as_view()),
-    path('projects/<int:pk>/',views.ProjectView.as_view()),
+    path('project/<int:pk>/',views.ProjectView.as_view()),
 
 ]
